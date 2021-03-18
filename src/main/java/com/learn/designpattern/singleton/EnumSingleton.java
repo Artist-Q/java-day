a2f3de63878b54df319c198fdb95b6d23aed046f
JeffreyTan <1550826232@qq.com>
2021-03-18 11:51:36 +0800

    添加单例设计模式

---
+++ src/main/java/com/learn/designpattern/singleton/EnumSingleton.java
@@ -0,0 +1,42 @@
+package com.learn.designpattern.singleton;
+
+/**
+ * 枚举方式
+ * <p>
+ * effective java中比较推荐的一种方式
+ * </p>
+ *
+ * @author Artis-Q
+ * @version 1.0, 24/02/2021
+ * @since 0.1.0
+ */
+public class EnumSingleton {
+
+    private EnumSingleton() {
+    }
+
+    private enum Singleton {
+
+        /** instance */
+        INSTANCE;
+
+        private final EnumSingleton instance;
+
+        Singleton() {
+            instance = new EnumSingleton();
+        }
+
+        private EnumSingleton getInstance() {
+            return instance;
+        }
+    }
+
+    /**
+     * 获取对象
+     *
+     * @return instance
+     */
+    public static EnumSingleton getInstance() {
+        return Singleton.INSTANCE.getInstance();
+    }
+}
---
+++ src/test/java/com/learn/javaday/JavaDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.javaday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
